bdfc440b22cf0b9ab3cc80449e41ec6440e4b40c
avaneesh2640 <avaneeshsharma9219@gmail.com>
2026-04-21 20:48:17 +0530

    Updated main, member3 analysis and member4 model

--- member2_feature_engineering.py
+++ member2_feature_engineering.py
@@ -1,27 +1,13 @@
 import pandas as pd
 
-# Load cleaned data (from Member 1 output)
-train = pd.read_csv("train_clean.csv")
-test = pd.read_csv("test_clean.csv")
+def feature_engineering(train, test):
 
-# Convert categorical to numeric
-train['Sex'] = train['Sex'].map({'male': 0, 'female': 1})
-test['Sex'] = test['Sex'].map({'male': 0, 'female': 1})
+    # Convert categorical to numeric
+    train['Sex'] = train['Sex'].map({'male': 0, 'female': 1})
+    test['Sex'] = test['Sex'].map({'male': 0, 'female': 1})
 
-# Convert Embarked column into numbers
-train = pd.get_dummies(train, columns=['Embarked'], drop_first=True)
-test = pd.get_dummies(test, columns=['Embarked'], drop_first=True)
+    # Handle missing values
+    train.fillna(0, inplace=True)
+    test.fillna(0, inplace=True)
 
-# Select important features
-features = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare']
-
-X = train[features]
-y = train['Survived']
-
-X_test = test[features]
-
-# Save processed data
-X.to_csv("X_train.csv", index=False)
-y.to_csv("y_train.csv", index=False)
-X_test.to_csv("X_test.csv", index=False)
-print("Feature engineering done ✅")
+    return train, test
