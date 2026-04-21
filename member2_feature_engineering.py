d753e0c45e3ecfd2f163f61cb61d9cc6eccc16ba
avaneesh2640 <avaneeshsharma9219@gmail.com>
2026-04-21 21:24:04 +0530

    Updated member2 feature engineering

--- member2_feature_engineering.py
+++ member2_feature_engineering.py
@@ -2,6 +2,10 @@ import pandas as pd
 
 def feature_engineering(train, test):
 
+    # Clean text (important fix)
+    train['Sex'] = train['Sex'].str.lower().str.strip()
+    test['Sex'] = test['Sex'].str.lower().str.strip()
+
     # Convert categorical to numeric
     train['Sex'] = train['Sex'].map({'male': 0, 'female': 1})
     test['Sex'] = test['Sex'].map({'male': 0, 'female': 1})
